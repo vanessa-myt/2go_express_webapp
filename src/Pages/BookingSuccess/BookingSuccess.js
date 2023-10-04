e06aab6a47b00d208b1ed9110fa76e2b126cade0
Vanessa Ibale <vanessaibale@Vanessas-MacBook-Air.local>
2023-10-04 16:44:24 +0800

    feat: booking success

--- src/Pages/BookingSuccess/BookingSuccess.js
+++ src/Pages/BookingSuccess/BookingSuccess.js
@@ -1,53 +1,67 @@
-import React, {useEffect, useState }  from 'react'
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../../Components/Navbar/Navbar'
-import { Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Navbar from "../../Components/Navbar/Navbar"
+import { Navigate } from "react-router-dom"
 
 import "./BookingSuccess.css"
-import { refreshPage } from '../../Helpers/Utils/Common';
+import { refreshPage } from "../../Helpers/Utils/Common"
 
-function BookingSuccess() {
-    const navigate = useNavigate()
-    const [redirect, setRedirect] = useState("");
-    if(redirect === "new") {
-        setTimeout(() => {
-            navigate("/")
-            refreshPage();
-          },1000);
-        
-    }
+function BookingSuccess({ navigation, transactionDetails, generalDetails }) {
+  const navigate = useNavigate()
+  const [redirect, setRedirect] = useState("")
+  if (redirect === "new") {
+    setTimeout(() => {
+      navigate("/")
+      refreshPage()
+    }, 1000)
+  }
 
+  console.log("transactionDetails", transactionDetails)
+  console.log("generalDetails", generalDetails)
 
-    return (
-        <div className='container'>
-            <Navbar></Navbar>
-                <h1 className="header justify-content-start title left mt-5">PLACE BOOKING</h1>
-                <div className="container form-cont">
-                    <h3 className="header justify-content-start title mt-3">BOOKING SUCCESSFUL</h3>
-                    <div className='row'>
-                        <div className='col'>
-                            <h3 className="header justify-content-start title mt-3 right grey">REFERENCE ID:</h3>
-                        </div>
-                        <div className='col'>
-                            <h3 className="header justify-content-start title mt-3 left">202200909909009123</h3>
-                        </div>
-                    </div>
-                    
-                    <div className="row justify-content-end mt-5">
-                        <div className="col-sm-2">
-                            <button className="btn-pink btn-rad" onClick={() => setRedirect("new")}> New </button>
-                            {/* onClick={()=>navigation.previous()} */}
-                        </div>
-                        <div className="col-sm-2">
-                                <button className="btn-blue btn-rad" type="submit" onClick={() => setRedirect("new")}> Home </button>
-                                {/* onClick={handleNext} */}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="container">
+        <h1 className="header justify-content-start title left mt-5">
+          PLACE BOOKING
+        </h1>
+        <div className="container form-cont">
+          <h3 className="header justify-content-start title mt-3">
+            BOOKING SUCCESSFUL
+          </h3>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center">
+              <h3 className="header title mt-3 grey">
+                TRANSACTION ID:{" "}
+                <span style={{ color: "var(--primary-color)" }}>
+                  {transactionDetails.transaction_id}
+                </span>
+              </h3>
+            </div>
+            <div className="col-12  text-center">
+              <span>
+                Please go to your nearest 2GO outlet to make the payment.
+              </span>
+            </div>
+          </div>
+
+          <div className="row justify-content-center mt-5">
+            <div className="col-sm-2">
+              <button
+                className="btn-pink btn-rad"
+                onClick={() => navigation.go("international")}
+              >
+                {" "}
+                New Transaction{" "}
+              </button>
+              {/* onClick={()=>navigation.previous()} */}
+            </div>
+          </div>
         </div>
-    )
-
+      </div>
+    </div>
+  )
 }
 
 export default BookingSuccess
-
