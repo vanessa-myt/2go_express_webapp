887b4870afdd3e3b103b4e2617bd616353b4d8a4
Vanessa Ibale <vanessaibale@Vanessas-MacBook-Air.local>
2023-10-11 16:53:17 +0800

    integ: package codes and add

--- src/Helpers/ApiCalls/expressAPI.jsx
+++ src/Helpers/ApiCalls/expressAPI.jsx
@@ -39,3 +39,102 @@ export const searchAreas = async (details, type) => {
     return { error: error.response }
   }
 }
+
+export const searchPackageCodes = async (details) => {
+  try {
+    var destination_system
+    if (details.destination_system === "ZIP-S") {
+      destination_system = "ZIPSS"
+    }
+    if (details.destination_system === "ZIP-R") {
+      destination_system = "ZIP"
+    }
+    if (details.destination_system === "QAIR") {
+      destination_system = "QAIR"
+    }
+
+    const response = await postAPICall(
+      process.env.REACT_APP_LINK + "express_package_codes/search",
+      {
+        shipment_mode: destination_system,
+        packaging: details.packaging,
+        transaction_type: details.transaction_type,
+      }
+    )
+
+    return { data: response.data.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
+
+export const createExpress = async (
+  data,
+  package_details,
+  package_codes,
+  expressType,
+  selectedOutlet
+) => {
+  try {
+    var paymode = ""
+    if (data.account_number === "WALK-IN") {
+      // data.is_walk_in = "1";
+      if (data.payor === "shipper") {
+        paymode = "cash"
+      }
+      if (data.payor === "consignee") {
+        paymode = "freight-collect"
+      }
+    } else {
+      if (data.payment_term === "credit") {
+        if (data.payor === "shipper") {
+          paymode = "collect-shipper"
+        }
+        if (data.payor === "consignee") {
+          paymode = "collect-consignee"
+        }
+      } else {
+        paymode = "cash"
+      }
+      // data.is_walk_in = "0";
+    }
+    var pkg_details = []
+    var stickers = []
+    package_details?.map((val) => {
+      val.port_sticker_numbers.map((sticker) => {
+        stickers.push(sticker.serial)
+      })
+      pkg_details.push({
+        ...val,
+        package_code: package_codes[parseInt(val.package_code)]["id"],
+        port_sticker_numbers: stickers,
+        commodity_code: val.commodity_code[0].code,
+      })
+
+      // val.package_code = package_codes[parseInt(val.package_code)]["code"];
+    })
+
+    const response = await postAPICall(
+      process.env.REACT_APP_LINK + "express_transactions/add",
+      {
+        ...data,
+        destination_system:
+          data.destination_system === "ZIP-R" ||
+          data.destination_system === "ZIP-S"
+            ? "ZIP"
+            : data.destination_system,
+        shipment_mode: data.destination_system,
+        paymode: paymode,
+        is_walk_in: "1",
+        package_details: [...pkg_details],
+        is_dropoff: "0",
+        save_shipper: data.save_shipper === true ? "1" : "0",
+        save_consignee: data.save_consignee === true ? "1" : "0",
+      }
+    )
+
+    return { data: response.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
